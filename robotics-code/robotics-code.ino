b7cf2dba126793744e4c4a9c6e3dba08fabc663a
Andy Lazcano <36870910+Alazca@users.noreply.github.com>
2023-09-30 21:00:19 +0000

    Update robotics-code.ino
    added filtering and error calculations.

--- robotics-code/robotics-code.ino
+++ robotics-code/robotics-code.ino
@@ -10,9 +10,10 @@ Paul McWhorter (YouTube)         -- General Arduino / Servo help
 howtomechanics.com               -- MPU6050, calcuating roll, pitch, yaw
 forum.arduino.cc                 -- General help with pinout / specifications
 Ian Carey (YouTube)              -- Integrating PID to arduino codebase 
+Phil's Lab (YouTube)             -- Error Filter for MPU
 Electronoobs (YouTube)           -- General microcontrollers
-
 ******************************/
+
 #include <Wire.h>
 #include <Servo.h>
 #include <math.h>
@@ -32,6 +33,7 @@ int loopDelay = 10;
 
 // MPU - Gyro variables 
 float rawGx, rawGy, rawGz = 0.0;
+float gyroErrorX, gyroErrorY, gyroErrorZ = 0.0;
 float gyroSense = 131;
 float thetaX, thetaY = 0.0;
 float errorAngleX, errorAngleY = 0.0;
@@ -39,6 +41,7 @@ int disDelay = 350;
 
 // MPU - Accel variables
 float rawAx, rawAy, rawAz = 0.0;
+float accErrorX, accErrorY, accErrorZ = 0.0;
 float phiX, phiY = 0.0;
 float accelSense = 16384;
 
@@ -47,10 +50,10 @@ float timeNow, timeThen, dt = 0.0;
 
 // Math variables and PID
 float proportional, integral, derivative = 0.0;
-float kp = 0.20;     // Needs to be small
-float ki = 0.03;     // Needs to be smallest
-float kd = 25.0;     // Needs to be biggest
-float toDeg = 180 * PI;
+float kp = 1.00;     // Needs to be small
+float ki = 0.00;     // Needs to be smallest
+float kd = 0.00;     // Needs to be biggest
+float toDeg = 180 / PI;
 float error, oldError, corrected = 0.0;
 float finalAngleX, finalAngleY = 0.0;
 
@@ -62,11 +65,15 @@ void rawMPU();
 void processMPU();
 void mtrctrl();
 void MPUdisplay();
+void MPUerror();
+
+
 
 void setup(){
     Serial.begin(BAUD);
     mtr.attach(servoPin);
     MPUtest();
+    MPUerror();
     servoTest();
 }
 
@@ -101,17 +108,6 @@ void servoTest(){
 }
 
 void rawMPU(){
-    // Raw gyro data communication
-    Wire.beginTransmission(MPUaddr);
-    Wire.write(0x43);
-    Wire.endTransmission();
-    Wire.requestFrom(MPUaddr, 6);
-    
-    // Converting the raw analogue values of the MPU based on datasheet specification
-    rawGx = ((Wire.read() << 8 | Wire.read())) / gyroSense;
-    rawGy = ((Wire.read() << 8 | Wire.read())) / gyroSense;
-    rawGz = ((Wire.read() << 8 | Wire.read())) / gyroSense;
-  
     // Raw accelerometer data communication
     Wire.beginTransmission(MPUaddr);
     Wire.write(0x3B);
@@ -122,16 +118,27 @@ void rawMPU(){
     rawAx = ((Wire.read() << 8 | Wire.read())) / accelSense;
     rawAy = ((Wire.read() << 8 | Wire.read())) / accelSense;
     rawAz = ((Wire.read() << 8 | Wire.read())) / accelSense;
+
+    // Raw gyro data communication
+    Wire.beginTransmission(MPUaddr);
+    Wire.write(0x43);
+    Wire.endTransmission();
+    Wire.requestFrom(MPUaddr, 6);
+    
+    // Converting the raw analogue values of the MPU based on datasheet specification
+    rawGx = ((Wire.read() << 8 | Wire.read())) / gyroSense;
+    rawGy = ((Wire.read() << 8 | Wire.read())) / gyroSense;
+    rawGz = ((Wire.read() << 8 | Wire.read())) / gyroSense;
 }
 
 void processMPU(){ 
     timeCtrl(); 
-    thetaX = rawGx * dt;
-    thetaY = rawGy * dt;
-    phiX = atan2(rawAy,         sqrt(pow(rawAx,2) + pow(rawAz,2))) * toDeg;
-    phiY = atan2(-1 * rawAx,    sqrt(pow(rawAy,2) + pow(rawAz,2))) * toDeg;
-    finalAngleX = (phiX + thetaX);
-    finalAngleY = (phiY + thetaY);
+    thetaX = (rawGx - gyroErrorX) * dt;
+    thetaY = (rawGy - gyroErrorY) * dt;
+    phiX = (atan2(rawAy,         sqrt(pow(rawAx,2) + pow(rawAz,2))) * toDeg) + accErrorY;
+    phiY = (atan2(-1 * rawAx,    sqrt(pow(rawAy,2) + pow(rawAz,2))) * toDeg) + accErrorY;
+    finalAngleX = (0.96 * phiX) + (0.04 * thetaX);
+    finalAngleY = (0.96 * phiY) + (0.04 * thetaY);
 }
 
 void timeCtrl(){
@@ -144,7 +151,7 @@ void mtrctrl(){
     actual = setPoint - finalAngleX;
     servoPos = pid(map(finalAngleX, -90, 90, 0, 180));   // References use +/- 170000 for mapping but not sure why
     if (servoPos != oldServo){
-      mtr.write(servoPos);
+      mtr.write(actual);
       oldServo = servoPos;
     }
     oldError = actual;
@@ -158,6 +165,41 @@ void MPUdisplay(){
     Serial.print(", Gyro Z = ");
     Serial.println(rawGz);
     delay(disDelay);
+}
+
+void MPUerror(){
+    Serial.println("Calibrating MPU - Recording Error. . .");
+    const int RANGE = 200;
+    for(int i = 0; i < RANGE; i++){
+      rawMPU();
+      accErrorX = atan2(rawAy,         sqrt(pow(rawAx,2) + pow(rawAz,2))) * toDeg;
+      accErrorY = atan2(-1 * rawAx,    sqrt(pow(rawAy,2) + pow(rawAz,2))) * toDeg;
+      i++;
+      delay(15);
+    }
+    accErrorX = accErrorX / RANGE;
+    accErrorY = accErrorY / RANGE;
+    for (int i = 0; i < RANGE; i++){
+      rawMPU();
+      gyroErrorX += rawGx; 
+      gyroErrorY += rawGy; 
+      gyroErrorZ += rawGz; 
+      i++;
+      delay(15);
+    }
+    gyroErrorX = gyroErrorX / RANGE;
+    gyroErrorY = gyroErrorY / RANGE;
+    gyroErrorZ = gyroErrorZ / RANGE;
+    Serial.print("Acceleration Error in X= ");
+    Serial.print(accErrorX);
+    Serial.print("\tAcceleration Error in Y: ");
+    Serial.print(accErrorY);
+    Serial.print("\nGyro Error in X: ");
+    Serial.print(gyroErrorX);
+    Serial.print("\tGyro Error in Y: ");
+    Serial.print(gyroErrorY);
+    Serial.print("\tGyro Error in Z: ");
+    Serial.println(gyroErrorZ);
 }
 
 float pid(float error){
